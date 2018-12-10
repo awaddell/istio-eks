36533a9f0deb3557a79df9116d56552b9e31c74d
Alastair Waddell <ali@isp20.com>
2018-12-11 10:39:23 +1100

    replace more fixed values with variables

--- dev/eks/main.tf
+++ dev/eks/main.tf
@@ -33,20 +33,20 @@ variable "aws_region" {}
 
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
-  version      = "1.8.0"
+  version      = "${var.eks_module_version}"
   cluster_name = "${var.cluster_name}"
   subnets      = ["${data.terraform_remote_state.vpc.private_subnets}"]
 
   workers_group_defaults = {
-    ami_id               = "ami-0f54a2f7d2e9c88b3" # AMI ID for the eks workers. If none is provided, Terraform will search for the latest version of their EKS optimized worker AMI.
-    asg_desired_capacity = "2"                     # Desired worker capacity in the autoscaling group.
-    asg_max_size         = "3"                     # Maximum worker capacity in the autoscaling group.
-    asg_min_size         = "1"                     # Minimum worker capacity in the autoscaling group.
-    instance_type        = "t2.small"              # Size of the workers instances.
-    key_name             = "eks-worker"            # The key name that should be used for the instances in the autoscaling group
-    additional_userdata  = ""                      # userdata to append to the default userdata.
-    ebs_optimized        = true                    # sets whether to use ebs optimization on supported types.
-    public_ip            = false                   # Associate a public ip address with a worker
+    ami_id               = "${var.eks_ami_id}"        # AMI ID for the eks workers. If none is provided, Terraform will search for the latest version of their EKS optimized worker AMI.
+    asg_desired_capacity = "2"                        # Desired worker capacity in the autoscaling group.
+    asg_max_size         = "3"                        # Maximum worker capacity in the autoscaling group.
+    asg_min_size         = "1"                        # Minimum worker capacity in the autoscaling group.
+    instance_type        = "${var.eks_instance_type}" # Size of the workers instances.
+    key_name             = "${var.eks_ssh_key}"       # The key name that should be used for the instances in the autoscaling group
+    additional_userdata  = ""                         # userdata to append to the default userdata.
+    ebs_optimized        = true                       # sets whether to use ebs optimization on supported types.
+    public_ip            = false                      # Associate a public ip address with a worker
   }
 
   tags = "${
--- dev/terraform.tfvars
+++ dev/terraform.tfvars
@@ -1,7 +1,23 @@
+# the aws region for the VPC and cluster
 aws_region = "us-west-2"
 
+# name to use to identify the cluster
 cluster_name = "demo-dev-cluster"
 
+# AMI ID for the eks workers (must be available in aws_region)
+eks_ami_id = "ami-0f54a2f7d2e9c88b3"
+
+# instance type for the eks workers
+eks_instance_type = "t2.small"
+
+# eks module version to download
+eks_module_version = "1.8.0"
+
+# ssh key (already uploaded to AWS) for the eks workers
+eks_ssh_key = "eks-worker"
+
+# environment (dev/prod)
 environment = "dev"
 
+# name to use to identify the VPC
 vpc_name = "demo-dev-vpc"
