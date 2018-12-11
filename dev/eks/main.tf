79abada206ebb79c20f1ef87d0545be7d353929c
Alastair Waddell <ali@isp20.com>
2018-12-11 12:20:49 +1100

    Squashed commit of the following:
    commit 125039424c83f7f98497efdf2b2d305424149281
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 11:43:17 2018 +1100
    
        include variable definitions
    
    commit 8f73659791dbd47c719be70a4e3344562fc1978a
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 11:42:55 2018 +1100
    
        cannot use variable for module version
    
    commit e604c2ec594b4336489894ba3938ffb8f94682e1
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 11:42:07 2018 +1100
    
        bump terraform version
    
    commit 129db6c231af3331c615981497860c31ba3951ec
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 11:41:51 2018 +1100
    
        remove backend local
    
    commit 07b60c3153aed18196dd4d01128b99e866fdef30
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 11:40:28 2018 +1100
    
        update readme
    
    commit 36533a9f0deb3557a79df9116d56552b9e31c74d
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 10:39:23 2018 +1100
    
        replace more fixed values with variables
    
    commit cf1029b304f7db3b9843b46879f4ef5faad3ef42
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 10:27:43 2018 +1100
    
        format the tfvars
    
    commit d4b59de7afc7f4414baad622a7708e5c2b7d75fb
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 10:24:33 2018 +1100
    
        terraform lint
    
    commit d65a2342f2ca81e68fb99c6cf93f1fc67c6eab16
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 10:23:18 2018 +1100
    
        pin versions in providers
    
    commit edb70251836c784482606896547153e033f2bed9
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 10:22:53 2018 +1100
    
        add variables and outputs
    
    commit 2d7c5a7137be02e25e42456cd8c8e28e070c7405
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Tue Dec 11 10:20:54 2018 +1100
    
        add local backend state and import variables from vpc
    
    commit 838db2590dce635e690f87cbf5eee22d6355eb6e
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Mon Dec 10 20:42:28 2018 +1100
    
        linting
    
    commit 4f8834538abd589292563aa35e97fc741717a068
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Mon Dec 10 20:39:04 2018 +1100
    
        migrate vpc and eks to distinct folders
    
    commit 5cbe0156b2ca956e25e13e70bcddb9bde48ae3e6
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Mon Dec 10 12:54:15 2018 +1100
    
        add a worker group with ami_id
    
    commit a971674dbe8ec9615a522a03602b9655dca8c92e
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Mon Dec 10 11:09:10 2018 +1100
    
        lint with terraform fmt
    
    commit 35b71d5b5c4b2264b9114fc9aafe41dcb9771c04
    Author: Alastair Waddell <ali@isp20.com>
    Date:   Sun Dec 9 20:23:01 2018 +1100
    
        add vpc and eks

---
+++ dev/eks/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_version = ">= 0.11.10"
+}
+
+provider "aws" {
+  region  = "${var.aws_region}"
+  version = "~> 1.51"
+}
+
+provider "local" {
+  version = "~> 1.1"
+}
+
+provider "null" {
+  version = "~> 1.0"
+}
+
+provider "template" {
+  version = "~> 1.0"
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config {
+    path = "../vpc/terraform.tfstate"
+  }
+}
+
+variable "cluster_name" {}
+variable "environment" {}
+variable "aws_region" {}
+variable "eks_ami_id" {}
+
+variable "eks_instance_type" {
+  default = "t2.micro"
+}
+
+variable "eks_ssh_key" {}
+
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  version      = "1.8.0"
+  cluster_name = "${var.cluster_name}"
+  subnets      = ["${data.terraform_remote_state.vpc.private_subnets}"]
+
+  workers_group_defaults = {
+    ami_id               = "${var.eks_ami_id}"        # AMI ID for the eks workers. If none is provided, Terraform will search for the latest version of their EKS optimized worker AMI.
+    asg_desired_capacity = "2"                        # Desired worker capacity in the autoscaling group.
+    asg_max_size         = "3"                        # Maximum worker capacity in the autoscaling group.
+    asg_min_size         = "1"                        # Minimum worker capacity in the autoscaling group.
+    instance_type        = "${var.eks_instance_type}" # Size of the workers instances.
+    key_name             = "${var.eks_ssh_key}"       # The key name that should be used for the instances in the autoscaling group
+    additional_userdata  = ""                         # userdata to append to the default userdata.
+    ebs_optimized        = true                       # sets whether to use ebs optimization on supported types.
+    public_ip            = false                      # Associate a public ip address with a worker
+  }
+
+  tags = "${
+    map(
+     "Name", "terraform-eks-demo-node",
+     "kubernetes.io/cluster/${var.cluster_name}", "shared",
+    )
+  }"
+
+  vpc_id          = "${data.terraform_remote_state.vpc.vpc_id}"
+  manage_aws_auth = true
+}
---
+++ dev/terraform.tfvars
@@ -0,0 +1,20 @@
+# the aws region for the VPC and cluster
+aws_region = "us-west-2"
+
+# name to use to identify the cluster
+cluster_name = "demo-dev-cluster"
+
+# AMI ID for the eks workers (must be available in aws_region)
+eks_ami_id = "ami-0f54a2f7d2e9c88b3"
+
+# instance type for the eks workers
+eks_instance_type = "t2.small"
+
+# ssh key (already uploaded to AWS) for the eks workers
+eks_ssh_key = "eks-worker"
+
+# environment (dev/prod)
+environment = "dev"
+
+# name to use to identify the VPC
+vpc_name = "demo-dev-vpc"
